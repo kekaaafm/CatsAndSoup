e5fa9d6d275129387a47b8a17def944195bfedf6
MaitreRouge <gazmarcou@outlook.fr>
2022-08-30 03:57:57 +0200

    Modifs visuelles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,8 +27,8 @@
         <th>Date</th>
         <th>Syleam</th>
         <th>Marketing</th>
-        <th>Diff tot</th>
         <th>Gain veille</th>
+        <th>Diff tot</th>
         <th></th>
         <th>Gain Syleam</th>
         <th>Gain Mark</th>
@@ -41,13 +41,13 @@
     </tr>
     </thead>
     <tbody>
-        @foreach($values->dataset as $value)
+    @foreach($values->dataset as $value)
         <tr>
             <td>{{ $value['date'] }}</td>
             <td style="color: @if($value['Syleam']>$value['Marketing']) #2ca02c @endif">{{ $value['Syleam'] }}</td>
             <td style="color: @if($value['Syleam']<$value['Marketing']) #2ca02c @endif">{{ $value['Marketing'] }}</td>
-            <td>{{ $value['diff_tot'] }}</td>
             <td style="color: @if(($value['gain_veille']??0)<0) #2ca02c @elseif(isset($value['gain_veille'])) #AA3333 @endif">{{ $value['gain_veille']??"-" }}</td>
+            <td>{{ $value['diff_tot'] }}</td>
             <td></td> {{-- Empty --}}
             <td style="color: @if(isset($value['gain_syleam']) && $value['gain_syleam']>$value['gain_marketing']) #2ca02c @endif">{{ $value['gain_syleam']??"-" }}</td>
             <td style="color: @if(isset($value['gain_syleam']) && $value['gain_syleam']<$value['gain_marketing']) #2ca02c @endif">{{ $value['gain_marketing']??"-" }}</td>
@@ -55,10 +55,16 @@
             <td>{{ isset($value['moy_3d_syleam'])?round($value['moy_3d_syleam'],2):"-" }}</td>
             <td>{{ isset($value['moy_3d_marketing'])?round($value['moy_3d_marketing'],2):"-" }}</td>
             <td></td> {{-- Empty --}}
-            <td>{{ isset($value['eta'])?round($value['eta'],2):"-" }}</td>
-            <td>{{ isset($value['eta_3d'])?round($value['eta_3d'],2):"-" }}</td>
+            <td style="color: @if(isset($value['eta']) and $value['eta']<0) #AA3333 @endif">{{ isset($value['eta'])?round($value['eta'],2):"-" }}</td>
+            <td style="color: @if(isset($value['eta_3d']) and $value['eta_3d']<0) #AA3333 @endif">{{ isset($value['eta_3d'])?round($value['eta_3d'],2):"-" }}</td>
         </tr>
-        @endforeach
+    @endforeach
+    <tr>
+        <td>Total</td>
+        <td></td>
+        <td></td>
+        <td style="color: @if($values->gainsTotaux<0) #2ca02c @else #AA3333 @endif">{{ $values->gainsTotaux }}</td>
+    </tr>
     </tbody>
 </table>
 
